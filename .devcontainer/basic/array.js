bfe1b7c203bd22a6f56d4f6822f8ffa693877280
vbhv2004 <141803701+vbhv2004@users.noreply.github.com>
2024-04-01 20:11:54 +0000

    array

---
+++ .devcontainer/basic/array.js
@@ -0,0 +1,25 @@
+const myArry = [0,1,2,3,4,5]
+
+// const myArry = new Array(0,1,2,3,4,5)
+
+
+// myArry.push(9)
+// myArry.pop(3)
+// myArry.pop(1)
+
+// console.log(myArry.includes(3));
+
+
+// console.log(myArry.includes(8));
+
+// const newArry = myArry.join()
+
+// console.log(newArry)
+
+// console.log(typeof newArry);
+
+const tArry = myArry.slice(1,4)
+
+console.log(tArry);
+
+// splice manupulate the original array and include last index 
